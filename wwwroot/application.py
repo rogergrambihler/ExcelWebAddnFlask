db37bced8d073250f588adee27a24f3fce54459d
Roger Grambilher <rogergrambihler@hotmail.com>
2019-02-05 08:57:53 -0800

    more rework

--- wwwroot/application.py
+++ wwwroot/application.py
@@ -3,7 +3,7 @@ This script runs the FlaskWebProject1 application using a development server.
 """
 
 from os import environ
-from FlaskWebProject1 import app
+from website import app
 
 
 if __name__ == '__main__':
--- wwwroot/website/views.py
+++ wwwroot/website/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from FlaskWebProject1 import app
+from website import app
 
 @app.route('/')
 @app.route('/home')
