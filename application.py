e06d03a422d5f542b04965135fda85a77923883f
Roger Grambihler <rogerg@microsoft.com>
2019-02-07 15:58:19 -0800

    rework for manifest

---
+++ application.py
@@ -0,0 +1,16 @@
+"""
+This script runs the FlaskWebProject1 application using a development server.
+"""
+
+from os import environ
+from website import app
+
+
+if __name__ == '__main__':
+
+    # If launched as main then setup the Flask Server
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    PORT = int(environ.get('SERVER_PORT', '50603'))
+
+    # https://localhost:50603
+    app.run(HOST, PORT, debug=True, ssl_context=('certs/server.crt', 'certs/server.key'))
